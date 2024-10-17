ec4446af91d8777a12c3e177bddf4445a8bc5f52
Jaanu12345 <75079880+Jaanu12345@users.noreply.github.com>
2024-10-17 13:00:31 +0530

    Add files via upload

---
+++ Authentication FLow/index.js
@@ -0,0 +1,84 @@
+<script src="https://www.google.com/recaptcha/api.js?render=explicit" defer async></script> 
+
+      // ensure namespace is available
+      Dribbble = window.Dribbble || {};
+      Dribbble.JsConfig = Dribbble.JsConfig || {};
+      Dribbble.isHistorySupported = () => (window.history && 'pushState' in window.history);
+    
+      User = {
+        loggedIn: function() {
+          return !!document.querySelector('body.logged-in')
+        },
+        loggedOut: function() {
+          return !this.loggedIn()
+        },
+      };
+
+
+if (typeof(Dribbble) != "undefined" && typeof(Dribbble.Shots) != "undefined") {
+    Dribbble.Shots.update(21444558, {
+    likes_count: 210,
+    liked: false
+    });
+}
+
+
+      var config = Dribbble.JsConfig || {}
+      // the assign is so this partial can be called in other partials, e.g. modals
+      // without overriding parent instantiations of JsConfig
+      Dribbble.JsConfig = Object.assign(config, {
+          user: {"isLoggedIn":false,"reCaptchaSiteKey":"6LdmBTIUAAAAAM4NIokaWu8p3BBuYEw3CxuDdyg_","canPurchasePro":true,"isHiringTrialEligible":false},
+          hiringProfile: {},
+          features: {"collaborators":false,"shotFeedUpdates":false,"caseStudiesLinkBlock":false,"marketplace":true},
+          isRobot: false,
+          remoteIp: "157.48.112.188",
+          isProduction: true,
+          permissions: {"userPermissions":["canBuyBoostWithBraintree","canSeeThirdPartyPaymentMethods"]},
+          screenshot_boost: {"buttonText":[{"label":"Buy Now","value":"buy-now"},{"label":"Download","value":"download"},{"label":"Learn More","value":"learn-more"},{"label":"Shop Now","value":"shop-now"},{"label":"Apply Now","value":"apply-now"},{"label":"Try Now","value":"try-now"},{"label":"Get Offer","value":"get-offer"},{"label":"Contact Us","value":"contact-us"}],"tiers":{"lowTier":{"daysToServe":7,"range":{"lowerLimit":0,"upperLimit":10000}},"midTier":{"daysToServe":14,"range":{"lowerLimit":10001,"upperLimit":100000}},"highTier":{"daysToServe":28,"range":{"lowerLimit":100001,"upperLimit":null}}},"pricePerImpression":"0.01","purchase":{"stripePublicKey":"pk_live_9EfFSEE6iTCgHghKqBqnixxR","savedCreditCardInformation":null},"discount":null,"minimumImpressions":2000,"targetPlacements":{"following":"Following Feed","popular":"Popular Feed","search":"Search Feed","goods":"Goods Feed","recent":"New \u0026 Noteworthy Feed","shot_modal":"Shot Modal","similar_work":"Similar Work","tags":"Tag Feed","popular_first":"Popular Feed First"},"priorities":["self_serve","sales","remnant","sales_priority"]},
+          braintree: {"customerId":null,"postalCode":null},
+          shotData: {"attachmentCount":0,"boost":{"id":null,"canBoostShot":false,"hasBoostedShots":false,"status":null,"statusReason":null,"statusReasonSentence":null,"clicksCount":null,"impressionsCount":null,"totalImpressions":null},"canCurrentUserComment":false,"canCurrentUserRebound":false,"commentsCount":12,"commentsDisabled":false,"contentBlocks":{"results":[{"id":2955174,"subjectType":"Screenshot","subjectId":21444558,"contentType":"text","position":1,"content":{"text":{"type":"doc","content":[{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"text":"I couldn’t resist bringing the desktop view of the Authentication screen to life using ","type":"text"},{"text":"Principle","type":"text","marks":[{"type":"link","attrs":{"href":"https://principleformac.com/","class":"css-4rbku5 css-18t94o4 css-901oao css-16my406 r-1cvl2hr r-1loqt21 r-poiln3 r-bcqeeo r-qvutc0","target":"_blank"}}]},{"text":" 💃.","type":"text"}]},{"type":"paragraph","attrs":{"textAlign":"left"}},{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"text":"👉 ","type":"text"},{"text":"Follow along the journey to redesign this Customer Portal on Twitter","type":"text","marks":[{"type":"link","attrs":{"href":"https://twitter.com/fhlcreative/status/1657092160255057936?s=20","class":null,"target":"_blank"}}]}]},{"type":"paragraph","attrs":{"textAlign":"left"}},{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"text":"--------------------","type":"text"}]},{"type":"paragraph","attrs":{"textAlign":"start"}},{"type":"paragraph","attrs":{"textAlign":"start"},"content":[{"text":"🚀 Let's create something big together! 🚀","type":"text"}]},{"type":"paragraph","attrs":{"textAlign":"start"},"content":[{"text":"Contact Unfold at ","type":"text"},{"text":"unfold.co/contact","type":"text","marks":[{"type":"link","attrs":{"href":"https://unfold.co/contact/?utm_source=dribbble\u0026utm_medium=shot\u0026utm_campaign=product\u0026utm_term=video\u0026utm_content=0515023-Regions+MyMortgage+video","class":null,"target":"_blank"}}]}]},{"type":"paragraph","attrs":{"textAlign":"start"},"content":[{"text":"Follow us on ","type":"text"},{"text":"Twitter","type":"text","marks":[{"type":"link","attrs":{"href":"https://twitter.com/unfoldco","class":null,"target":"_blank"}}]},{"text":" | ","type":"text"},{"text":"Behance","type":"text","marks":[{"type":"link","attrs":{"href":"https://www.behance.net/unfoldcoteam","class":null,"target":"_blank"}}]},{"text":" | ","type":"text"},{"text":"Instagram","type":"text","marks":[{"type":"link","attrs":{"href":"https://www.instagram.com/unfoldco/","class":null,"target":"_blank"}}]},{"text":" | ","type":"text"},{"text":"YouTube","type":"text","marks":[{"type":"link","attrs":{"href":"https://www.youtube.com/channel/UCNV_rUx7S8SWlRbPLJAnVsg/featured","class":null,"target":"_blank"}}]}]},{"type":"paragraph","attrs":{"textAlign":"start"},"content":[{"text":"More about us on ","type":"text"},{"text":"unfold.co","type":"text","marks":[{"type":"link","attrs":{"href":"https://unfold.co/?utm_source=dribbble\u0026utm_medium=shot\u0026utm_campaign=product\u0026utm_term=video\u0026utm_content=0515023-Regions+MyMortgage+video","class":null,"target":"_blank"}}]}]}]},"isShotDescription":true},"userUploadsAttributes":[],"isCaseStudy":null},{"id":2955175,"subjectType":"Screenshot","subjectId":21444558,"contentType":"video","position":0,"content":{"alt":"","media":[{"id":"eef716ac-e79e-4006-9bc9-0794155a67d3","type":"video","width":3200,"height":2400,"fileExtension":"mp4","shrineCacheId":"cc3ddee1-8274-4c5c-b712-68a84fb1c103/regionsdribble-alt.mp4"}],"mediaId":"eef716ac-e79e-4006-9bc9-0794155a67d3","isFullWidth":true,"shouldVideoLoop":true,"shouldVideoAutoplay":true},"userUploadsAttributes":[{"cropData":{},"id":6957640,"position":0,"shrineCacheId":"cc3ddee1-8274-4c5c-b712-68a84fb1c103/regionsdribble-alt.mp4","fileDetails":{"large":{"id":"userupload/6957640/file/large-f9919911ac0ada6f762f748c97197bb7.mp4","storage":"store","metadata":{"size":431531,"width":800,"height":600,"filename":"large-thumbnail20230512-2491-u0n90s.mp4","mimeType":"video/mp4"}},"small":{"id":"userupload/6957640/file/small-9780116354ce3302ace7aaa7cdf2cdfb.mp4","storage":"store","metadata":{"size":161268,"width":400,"height":300,"filename":"small-thumbnail20230512-2491-1nid9nw.mp4","mimeType":"video/mp4"}},"still":{"id":"userupload/6957640/file/still-58c9b92308d18b1d382d41a79dbdc364.png","storage":"store","metadata":{"size":4483107,"width":3200,"height":2400,"filename":"screenshot20230512-2491-1ytccsy.png","mimeType":"image/png"}},"original":{"id":"userupload/6957640/file/original-de16d8e68a05acd8899010883d608823.mp4","storage":"store","metadata":{"size":12688199,"width":3200,"height":2400,"filename":null,"mimeType":"video/mp4"}}},"originalUrl":"https://cdn.dribbble.com/userupload/6957640/file/original-de16d8e68a05acd8899010883d608823.mp4","stillUrl":"https://cdn.dribbble.com/userupload/6957640/file/still-58c9b92308d18b1d382d41a79dbdc364.png","largeVideoUrl":"https://cdn.dribbble.com/userupload/6957640/file/large-f9919911ac0ada6f762f748c97197bb7.mp4","smallVideoUrl":"https://cdn.dribbble.com/userupload/6957640/file/small-9780116354ce3302ace7aaa7cdf2cdfb.mp4","height":2400,"width":3200}],"isCaseStudy":null}],"total":2},"featureShotUrl":"https://cdn.dribbble.com/userupload/6957639/file/still-a334911e4eb1d3fa695e24e8071bca5b.png","hasDownloads":false,"hasGood":false,"height":2400,"isAnimated":true,"isAnimatedGif":false,"isVideo":true,"isFromModal":false,"isLiked":null,"isMultiShot":true,"isOwnedByCurrentUser":false,"isPublished":true,"isPublishedByTeam":false,"isSaved":null,"likesCount":210,"postedOn":"May 16, 2023","reboundPath":"/uploads/new?rebound=21444558-Authentication-Flow","savesCount":68,"shareUtms":{"source":"Shot","campaign":"fhlcreative","content":"Authentication Flow"},"shotHasRebounds":false,"shotId":21444558,"shotMediaPreview":{"mediaType":"video","shotImageUrl":"https://cdn.dribbble.com/userupload/6957639/file/still-a334911e4eb1d3fa695e24e8071bca5b.png?resize=400x300","shotGifUrl":"https://cdn.dribbble.com/userupload/6957639/file/still-a334911e4eb1d3fa695e24e8071bca5b.png?resize=3200x2400","shotVideoUrl":"https://cdn.dribbble.com/userupload/6957639/file/small-90533148aeb0103fea873aa1629088aa.mp4"},"shotUser":{"id":97388,"forHire":true,"username":"fhlcreative","name":"Phil Goodwin","isTeam":false,"avatarUrl":"https://cdn.dribbble.com/users/97388/avatars/normal/c53cbdc951c2cd1de4644c03440c6f19.png?1684329994","url":"/fhlcreative","team":{"avatarUrl":"https://cdn.dribbble.com/users/17543311/avatars/normal/c9c68958672969d58b038cf8d2447c05.png?1694804202","id":17543311,"name":"Unfold","url":"/unfold"}},"tags":["bank app","finance app","banking","interface design","app design","desktop app","prototype","micro interaction","animation","sign in","login","web design","web","web app","software","uiux","ux","ui","interface","product"],"tagUrls":[{"name":"bank app","url":"https://dribbble.com/tags/bank-app"},{"name":"finance app","url":"https://dribbble.com/tags/finance-app"},{"name":"banking","url":"https://dribbble.com/tags/banking"},{"name":"interface design","url":"https://dribbble.com/tags/interface-design"},{"name":"app design","url":"https://dribbble.com/tags/app-design"},{"name":"desktop app","url":"https://dribbble.com/tags/desktop-app"},{"name":"prototype","url":"https://dribbble.com/tags/prototype"},{"name":"micro interaction","url":"https://dribbble.com/tags/micro-interaction"},{"name":"animation","url":"https://dribbble.com/tags/animation"},{"name":"sign in","url":"https://dribbble.com/tags/sign-in"},{"name":"login","url":"https://dribbble.com/tags/login"},{"name":"web design","url":"https://dribbble.com/tags/web-design"},{"name":"web","url":"https://dribbble.com/tags/web"},{"name":"web app","url":"https://dribbble.com/tags/web-app"},{"name":"software","url":"https://dribbble.com/tags/software"},{"name":"uiux","url":"https://dribbble.com/tags/uiux"},{"name":"ux","url":"https://dribbble.com/tags/ux"},{"name":"ui","url":"https://dribbble.com/tags/ui"},{"name":"interface","url":"https://dribbble.com/tags/interface"},{"name":"product","url":"https://dribbble.com/tags/product"}],"viewsCount":94970,"width":3200,"title":"Authentication Flow","shouldDisplayAds":true,"isCaseStudy":false,"shotFormat":"video"},
+      })
+
+
+      // Initialize multi-shot
+      Dribbble.MediaGallery.initialize();
+    
+      new Dribbble.VideoPlayer('.video-wrap video');
+    
+      // Move this logic to gallery
+    
+      // Move this logic to gallery
+      Dribbble.Shots.add([{
+        id: '21444558',
+        ga: []
+      }])
+    
+        new Dribbble.Bucket({ target: "a[data-bucket-add]" });
+    
+        Dribbble.AnimatedGifSwap.attach(".dribbble.gif", {
+          parentSelector: ".js-thumbnail:first",
+          picSelector: '.dribbble-shot picture'
+        });
+    
+      if (Dribbble.MediaSwap) {
+        new Dribbble.MediaSwap('[data-video-teaser-small][data-video-teaser-large]');
+      }
+    
+    
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+        // Google Analytics
+        gtag('config', 'G-EV4S8HEMZG', {
+          user_id: null,
+          "user_properties": {
+            anonymous_id: "0e673582-ab2e-491d-ad74-b3e68a28178a",
+            user_id: null,
+            user_type: "guest",
+          },
+        });
+    
+        // Google Ads
+        gtag('config', 'AW-787470371');
+    
